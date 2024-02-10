c76b576c45727a3797714af1791d7ec1607f23bb
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2024-02-10 13:13:09 +0100

    First commit

---
+++ src/main/java/com/banco/bancodelapeseta/BancodelapesetaApplication.java
@@ -0,0 +1,13 @@
+package com.banco.bancodelapeseta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodelapesetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancodelapesetaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banco/bancodelapeseta/BancodelapesetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.bancodelapeseta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodelapesetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
