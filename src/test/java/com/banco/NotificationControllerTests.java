c54601e0813507cb0a2f7e07097822af322a9cce
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2024-02-20 01:41:45 +0100

    updated test

--- src/test/java/com/banco/NotificationControllerTests.java
+++ src/test/java/com/banco/NotificationControllerTests.java
@@ -1,15 +1,25 @@
 package com.banco;
 
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.util.StringInputStream;
 import com.banco.dtos.AuthenticationRequestDto;
 import com.banco.entities.Entity;
 import com.banco.repositories.EntityRepository;
 import com.banco.security.JwtService;
+import com.banco.services.NotificationService;
+import com.banco.services.NotificationServiceImpl;
 import jakarta.transaction.Transactional;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,9 +43,12 @@ import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
 import software.amazon.awssdk.services.sns.model.PublishResponse;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -56,6 +69,9 @@ public class NotificationControllerTests {
     private SnsClient snsClient;
     @Autowired
     private JwtService jwtService;
+    @MockBean
+    private AmazonS3 s3Client;
+
 
 
 
@@ -64,6 +80,13 @@ public class NotificationControllerTests {
     @Test
     @Transactional
     public void testSimpleEmailServiceOk() throws Exception {
+        String property = "header<body>body</body>trailer";
+        InputStream testInputStream = new StringInputStream(property);
+        S3Object s3Object = new S3Object();
+        s3Object.setObjectContent(testInputStream);
+        when(s3Client.getObject((String) any(),any())).thenReturn(s3Object);
+
+
         when(amazonSimpleEmailService.sendEmail(any())).thenReturn(null);
         when(entityRepository.findByTaxId(any())).thenReturn(Optional.of(Entity.builder().emailConfirmed(false).phoneConfirmed(false).build()));
         mockMvc.perform(MockMvcRequestBuilders
