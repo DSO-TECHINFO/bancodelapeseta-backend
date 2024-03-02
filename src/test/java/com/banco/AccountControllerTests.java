2c52ab51d853d6b89e2c62e4cd912593b3c991ee
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2024-03-02 22:50:56 +0100

    updated test

--- src/test/java/com/banco/AccountControllerTests.java
+++ src/test/java/com/banco/AccountControllerTests.java
@@ -1,9 +1,12 @@
 package com.banco;
 
+import com.banco.dtos.CreateNewAccountDto;
 import com.banco.dtos.PasswordChangeDto;
+import com.banco.dtos.PhoneChangeDto;
 import com.banco.entities.Entity;
 import com.banco.repositories.EntityRepository;
 import com.banco.security.JwtService;
+import com.banco.services.VerifyService;
 import com.banco.utils.EntityUtils;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
@@ -15,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -40,6 +44,8 @@ public class AccountControllerTests {
     private MockMvc mockMvc;
     @Autowired
     private JwtService jwtService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     @WithMockUser
@@ -76,5 +82,28 @@ public class AccountControllerTests {
                         .get("/accounts").contentType(MediaType.APPLICATION_JSON)
                         .header("Authorization","Bearer " + jwtService.generateToken(new Entity())))
                 .andExpect(status().isForbidden());
+    }
+    @Test
+    @WithMockUser
+    @Transactional
+    public void testCreateAccountTestIsOk() throws Exception {
+        String mockedCode = "CODETEST";
+        when(entityRepository.findByTaxId(any()))
+                .thenReturn(Optional.of(Entity.builder()
+                        .id(18L)
+                        .emailConfirmed(true)
+                        .phoneConfirmed(true)
+                        .verifyWithSign(true)
+                        .verifyTransactionCode(passwordEncoder.encode(mockedCode))
+                        .verifyTransactionCodeAttempts(0)
+                        .verifyTransactionCodeExpiration(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(10)))
+                        .build()));
+
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/accounts/create").contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtils.asJsonString(CreateNewAccountDto.builder().currency("EUR").productId(2L).verificationCode(mockedCode).build()))
+                        .header("Authorization","Bearer " + jwtService.generateToken(new Entity())))
+                .andExpect(status().isCreated());
     }
 }
