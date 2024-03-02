73e088964e2493701a071fa7ba87c919bd55acdc
Jesus Martinez <74986138+jm-jesusm@users.noreply.github.com>
2024-03-02 23:41:24 +0100

    Card entity updated

--- src/main/java/com/banco/entities/Card.java
+++ src/main/java/com/banco/entities/Card.java
@@ -1,12 +1,16 @@
 package com.banco.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 import java.util.*;
 
 @Entity
@@ -15,17 +19,10 @@ import java.util.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Card {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @JsonIgnore
-    @OneToOne
-    private Contract contract;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    @Column
-    private String cvv;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Column
@@ -34,20 +31,44 @@ public class Card {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Column
     private String expiration;
-
-    @JsonIgnore
+    
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Column
-    private Double cashier_limit;
+    private String cvv;
 
-    @JsonIgnore
-    @Column
-    private Double daily_buyout_limit;
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    @Column(precision = 10, scale = 2)
+    private BigDecimal cashierLimit;
 
-    @JsonIgnore
-    @Column
-    private Short activated;
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    @Column(precision = 10, scale = 2)
+    private BigDecimal dailyBuyoutLimit;
 
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Column
-    private Date activation_date;
+    private Boolean activated;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column
+    private Date activationDate;
+    
+    @JsonIgnore
+    @OneToOne
+    private Contract contract;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column(name="type")
+    private CardType cardType;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column(precision = 10, scale = 2)
+    private BigDecimal chargedAmount;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column
+    private String pin;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column(precision = 10, scale = 2)
+    private BigDecimal fee;
 }
