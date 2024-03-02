6fc84a1132ab4c644f99357e705b421f2fa6b665
tamed <tadeo.s.amed@accenture.com>
2024-03-01 23:15:25 -0300

    feat: sets the scale and rounding mode for the LoanPayment attributes

--- src/main/java/com/banco/services/CalculateAmortizationService.java
+++ src/main/java/com/banco/services/CalculateAmortizationService.java
@@ -14,17 +14,17 @@ public class CalculateAmortizationService {
     public List<LoanPayment> calculateAmortization(Loan loan) {
         List<LoanPayment> payments = new ArrayList();
         BigDecimal remainingBalance = loan.getAmount();
-        BigDecimal monthlyInterestRate = loan.getInterestRate().divide(BigDecimal.valueOf(12), 3, RoundingMode.HALF_UP);
+        BigDecimal monthlyInterestRate = loan.getInterestRate().divide(BigDecimal.valueOf(12), 7, RoundingMode.HALF_UP);
         int totalPayments = loan.getLoanNumberPayments();
         BigDecimal monthlyPayment = this.calculateMonthlyPayment(loan.getAmount(), monthlyInterestRate, totalPayments);
 
         for(int i = 0; i < totalPayments; ++i) {
-            BigDecimal interestPayment = remainingBalance.multiply(monthlyInterestRate);
-            BigDecimal principalPayment = monthlyPayment.subtract(interestPayment);
-            remainingBalance = remainingBalance.subtract(principalPayment);
+            BigDecimal interestPayment = remainingBalance.multiply(monthlyInterestRate).setScale(2, RoundingMode.HALF_UP);
+            BigDecimal principalPayment = monthlyPayment.subtract(interestPayment).setScale(2, RoundingMode.HALF_UP);
+            remainingBalance = remainingBalance.subtract(principalPayment).setScale(2, RoundingMode.HALF_UP);
             LoanPayment payment = new LoanPayment();
             payment.setPaymentNumber(i + 1);
-            payment.setPaymentAmount(monthlyPayment);
+            payment.setPaymentAmount(monthlyPayment.setScale(2, RoundingMode.HALF_UP));
             payment.setInterestPaid(interestPayment);
             payment.setPrincipalPaid(principalPayment);
             payment.setRemainingBalance(remainingBalance);
@@ -37,6 +37,6 @@ public class CalculateAmortizationService {
     private BigDecimal calculateMonthlyPayment(BigDecimal loanAmount, BigDecimal monthlyInterestRate, int totalPayments) {
         BigDecimal numerator = loanAmount.multiply(monthlyInterestRate).multiply((BigDecimal.ONE.add(monthlyInterestRate)).pow(totalPayments));
         BigDecimal denominator = ((BigDecimal.ONE.add(monthlyInterestRate)).pow(totalPayments)).subtract(BigDecimal.ONE);
-        return numerator.divide(denominator, 3, RoundingMode.HALF_UP);
+        return numerator.divide(denominator, 7, RoundingMode.HALF_UP).setScale(2, RoundingMode.HALF_UP);
     }
 }
