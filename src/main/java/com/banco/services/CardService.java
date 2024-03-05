057a833bae4872c8ac0dc38f047cbede1dc7d180
Jesus Martinez <74986138+jm-jesusm@users.noreply.github.com>
2024-03-05 14:25:13 +0100

    CardService static methods deleted

--- src/main/java/com/banco/services/CardService.java
+++ src/main/java/com/banco/services/CardService.java
@@ -10,27 +10,4 @@ import com.banco.exceptions.CustomException;
 
 public interface CardService {
     List<EntityContract> getUserCards() throws CustomException; 
-
-    static List<CardDto> cardListToCardDtoList(List<Card> cardList) {
-        List<CardDto> cardDtoList = new ArrayList<>();
-        for(Card card : cardList) {
-            cardDtoList.add(cardToCardDto(card));
-        }
-        return cardDtoList;
-    }
-
-    static CardDto cardToCardDto(Card card) {
-        return CardDto.builder()
-                .id(card.getId())
-                .number(card.getNumber())
-                .expiration(card.getExpiration())
-                .cashierLimit(card.getCashierLimit())
-                .dailyBuyoutLimit(card.getDailyBuyoutLimit())
-                .activated(card.getActivated())
-                .activationDate(card.getActivationDate())
-                .cardType(card.getCardType())
-                .chargedAmount(card.getChargedAmount())
-                .fee(card.getFee())
-                .build();
-    }
 }
