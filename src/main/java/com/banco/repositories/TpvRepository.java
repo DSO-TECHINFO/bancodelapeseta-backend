d6878ba78bb4adf1b2316b03136b36ca28e744c3
yeramontes <yeramontes88@gmail.com0>
2024-03-07 20:11:20 +0100

    Creado TpvRepository base. #Tpv

---
+++ src/main/java/com/banco/repositories/TpvRepository.java
@@ -0,0 +1,9 @@
+package com.banco.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.banco.entities.Tpv;
+
+public interface TpvRepository extends JpaRepository<Tpv, Long> {
+    
+}
