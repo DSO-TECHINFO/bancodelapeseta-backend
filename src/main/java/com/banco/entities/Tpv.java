f3bdffe25b6ddf25262e43cd5e2aa1ac520d025b
rvumbra <rvumbra@gmail.com>
2024-03-07 21:23:41 +0100

    added tpv entity

--- src/main/java/com/banco/entities/Tpv.java
+++ src/main/java/com/banco/entities/Tpv.java
@@ -1,6 +1,8 @@
 package com.banco.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
@@ -8,7 +10,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
+/**
+ * <h1>Entidad PTV</h1>
+ * 
+ * <p>Esta entidad representa un terminal de punto de venta (TPV) como sistema
+ * de pagos en línea dependiente de los contratos.</p>
+ * 
+ * <h3>Propiedades</h3>
+ * <ul>
+    * <li><b>id:</b> Identificador único de la PTV</li>
+    * <li><b>serialNumber:</b> Número de serie</li>
+    * <li><b>tpvCode:</b> Código de desbloqueo</li>
+    * <li><b>activationDate:</b> Fecha de última activación</li>
+    * <li><b>activationDate:</b> Fecha de última desactivación</li>
+    * <li><b>activated:</b> Flag que indica si está activada la PTV</li>
+    * <li><b>contract:</b> Cuando lo sepamos te decimos máquina T_T</li>
+ * </ul>
+ */
 @Entity
+@Table(name = "tpv")
 @Data
 @Builder
 @AllArgsConstructor
@@ -18,7 +40,26 @@ public class Tpv {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column
+    private String serialNumber;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column
+    private String tpvCode;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column
+    private Date activationDate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column
+    private Date deactivationDate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Boolean activated;
+
     @JsonIgnore
-    @OneToOne(mappedBy = "tpv")
+    @OneToOne(mappedBy = "contractId")
     private Contract contract;
 }
