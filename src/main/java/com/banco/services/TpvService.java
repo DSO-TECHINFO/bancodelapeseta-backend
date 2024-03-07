8d3048b36a926b8c8a333782b7dbefe272cd687e
yeramontes <yeramontes88@gmail.com0>
2024-03-07 20:17:22 +0100

    Creados interface y impl base de Tpv. #Tpv

---
+++ src/main/java/com/banco/services/TpvService.java
@@ -0,0 +1,5 @@
+package com.banco.services;
+
+public interface TpvService {
+    
+}
---
+++ src/main/java/com/banco/services/TpvServiceImpl.java
@@ -0,0 +1,13 @@
+package com.banco.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.banco.repositories.TpvRepository;
+
+@Service
+public class TpvServiceImpl implements TpvService {
+    
+    @Autowired
+    private TpvRepository tpvRepository;
+}
