bcd0215a3d7a19a9ec53d392d7f6f91efc5190b5
tamed <tadeo.s.amed@accenture.com>
2024-03-13 14:46:39 -0300

    feat: adds logic to create a Loan

--- src/main/java/com/banco/services/CreateLoanService.java
+++ src/main/java/com/banco/services/CreateLoanService.java
@@ -6,13 +6,6 @@ import com.banco.entities.Loan;
 
 public interface CreateLoanService {
 
-    //crear funcionalidad para ir actualizando la entidad Loan luego de cada amortizacion.
-    // tengo que guardar el prestamos en la tabla prestamo y en la TABLA CONTRATO . En la tabla contract tengo que crear unregistro nuevo donde ponga el mismo accoountId y guardar el loan_id en el cmapo loan_id
-
     public void loanCreation(LoanRequestDto loanRequestDto);
-        // primero obtener el usuario para obtener el account
-        // mappear de LoanDTO a loan.
-        // luego calcular la amortizacion
-        //
 
 }
