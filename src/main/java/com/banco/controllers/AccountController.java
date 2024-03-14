11f71364c415a4bc983b49b09af55e362e3f40e0
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2024-03-14 16:04:52 +0100

    updated

--- src/main/java/com/banco/controllers/AccountController.java
+++ src/main/java/com/banco/controllers/AccountController.java
@@ -2,6 +2,7 @@ package com.banco.controllers;
 
 import com.banco.dtos.AddIntervenerToAccountDto;
 import com.banco.dtos.CreateNewAccountDto;
+import com.banco.dtos.RemoveIntervenerFromAccountDto;
 import com.banco.dtos.VerificationCodeDto;
 import com.banco.exceptions.CustomException;
 import com.banco.services.AccountService;
@@ -34,6 +35,11 @@ public class AccountController {
     @PostMapping("/add/intervener")
     public ResponseEntity<?> addIntervenerToAccount(@RequestBody AddIntervenerToAccountDto addIntervenerToAccountDto){
         accountService.addIntervenerToAccount(addIntervenerToAccountDto);
-        ResponseEntity.ok().build();
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/remove/intervener")
+    public ResponseEntity<?> removeIntervenerFromAccount(@RequestBody RemoveIntervenerFromAccountDto removeIntervenerFromAccountDto){
+        accountService.removeIntervenerFromAccount(removeIntervenerFromAccountDto);
+        return ResponseEntity.ok().build();
     }
 }
