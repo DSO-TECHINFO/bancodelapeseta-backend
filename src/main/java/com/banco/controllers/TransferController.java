6db3668b40e68fc84436875f1ef50a7edfea1b73
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2024-03-27 11:32:29 +0100

    update

--- src/main/java/com/banco/controllers/TransferController.java
+++ src/main/java/com/banco/controllers/TransferController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//asda
 @RestController
 @RequestMapping(path = "/transfers")
 @AllArgsConstructor
