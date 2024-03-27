f2d1ad838a3f57dfe0ea6c72a2319c4a19e634fb
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2024-03-27 11:26:59 +0100

    update

--- src/main/java/com/banco/controllers/TransferController.java
+++ src/main/java/com/banco/controllers/TransferController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//TransferController
 @RestController
 @RequestMapping(path = "/transfers")
 @AllArgsConstructor
---
+++ src/main/java/com/banco/scheduling/LoanScheduling.java
@@ -0,0 +1,11 @@
+package com.banco.scheduling;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class LoanScheduling {
+
+
+}
